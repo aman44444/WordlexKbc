4dd17252561cff9c5d9e2a8e5f169bce28e47762
aman1357 <amancorp04@gmail.com>
2024-06-25 23:45:38 +0530

    .

--- src/components/modals/Instructions.js
+++ src/components/modals/Instructions.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "../../style/./Instructions.css"
 
 const Instructions = ({close}) => {
 
-    function handleClick(event) {
+   const handleClick = (event) =>  {
         event.stopPropagation();
     }
+
     return (
         <div className='modal' onClick={handleClick}>
         <div className='overlay'>
@@ -15,23 +17,23 @@ const Instructions = ({close}) => {
             <p>HOW TO PLAY</p>
                 <section>
                     <div>
-                        <p>
-                            "Guess the"
-                            <strong>WORDLE</strong>
-                            "in 6 tries"
-                        </p>
-                        <p>Each guess must be a valid 5-letter word. Hit the enter button to submit.</p>
-                        <p>After each guess, the color of the tiles will change to show how close your guess was to the word.</p>
-                        <p>Its a 7 days game play if loose you will have to start from day 1. There will be a prize after day 3 ,day 5 and day 7</p>
-                        <div>
-                          
-                        </div>
-                        <p><strong>Lifelines</strong></p>
-                       
-                        <p><strong>50:50</strong>  :If use the 50:50 lifeline you will get the half word of word of the day .You can use this lifeline only once a in 7 days game.</p>
-                        <p><strong>FLIP</strong>  :If You use FLIP lifeline the word of the day will be change and you will get clean board.You can use this lifeline it only once in a 7 days game.</p>
-                        <p><strong>50:50</strong>  :If use the DD lifeline you will get one more chance to guess the word.You can use this only once  in  a 7 days game.</p><br/>
-                        <p><strong>A new WORDLE will be available each day!</strong></p>
+                     
+                    <p>
+                                <strong>Objective:</strong> Guess the <strong>WORDLE</strong> in 6 tries.
+                            </p>
+                            <p>Each guess must be a valid 5-letter word. Hit the Enter button to submit.</p>
+                            <p>After each guess, the color of the tiles will change to show how close your guess was to the word.</p>
+                            
+                            <h3>Gameplay:</h3>
+                            <p>This is a 7-day game. If you lose, you will have to start from day 1. There will be prizes after completing day 3, day 5, and day 7.</p>
+                            
+                            <h3>Lifelines:</h3>
+                            <p><strong>50:50:</strong> Get half of the word of the day revealed. You can use this lifeline only once in a 7-day game.</p>
+                            <p><strong>FLIP:</strong> Change the word of the day and get a clean board. You can use this lifeline only once in a 7-day game.</p>
+                            <p><strong>Double Dip (DD):</strong> Get one more chance to guess the word. You can use this lifeline only once in a 7-day game.</p>
+                            
+                            <h3>New Word:</h3>
+                            <p><strong>A new WORDLE will be available each day!</strong></p>
                     </div>
                 </section>
             </div>
