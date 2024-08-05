0c2431f2f099e953d6fb100ccf0f782093311e40
aman1357 <amancorp04@gmail.com>
2024-08-05 21:03:03 +0530

    instructions

--- src/components/modals/Instructions.js
+++ src/components/modals/Instructions.js
@@ -21,6 +21,14 @@ const Instructions = ({ close }) => {
           <p>HOW TO PLAY</p>
           <section>
             <div>
+            <p>
+                <strong>About the Game:</strong> This game is a unique mix of{" "}
+                <strong>Wordle</strong> and the popular Indian television show{" "}
+                <strong>Kaun Banega Crorepati (KBC)</strong>, which is the
+                Indian version of "Who Wants to Be a Millionaire." Your goal is
+                to guess the correct word while using lifelines inspired by KBC
+                to help you along the way.
+              </p>
               <p>
                 <strong>Objective:</strong> Guess the <strong>WORDLE</strong> in
                 6 tries.
@@ -34,14 +42,14 @@ const Instructions = ({ close }) => {
                 close your guess was to the word.
               </p>
 
-              <h3>Gameplay:</h3>
+              <h2>Gameplay:</h2>
               <p>
                 This is a 7-day game. If you lose, you will have to start from
                 day 1. There will be prizes after completing day 3, day 5, and
                 day 7.
               </p>
 
-              <h3>Lifelines:</h3>
+              <h2>Lifelines:</h2>
               <p>
                 <strong>50:50:</strong> Get half of the word of the day
                 revealed. You can use this lifeline only once in a 7-day game.
@@ -56,7 +64,7 @@ const Instructions = ({ close }) => {
                 the word. You can use this lifeline only once in a 7-day game.
               </p>
 
-              <h3>New Word:</h3>
+              <h2>New Word:</h2>
               <p>
                 <strong>A new WORDLE will be available each day!</strong>
               </p>
