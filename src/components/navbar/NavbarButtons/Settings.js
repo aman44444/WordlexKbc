6432acd6fd063f4b074bb85901e0f64dbac850e0
aman1357 <amancorp04@gmail.com>
2025-10-02 14:01:31 +0530

    settings component

---
+++ src/components/navbar/NavbarButtons/Settings.js
@@ -0,0 +1,12 @@
+import { CiSettings } from "react-icons/ci";
+
+const Settings = () => {
+    return (
+        <div className="icons">
+           <CiSettings />
+        </div>
+
+    )
+}
+
+export default Settings;
