df28ff2f794bdd984946d59711a6e8c3ad3470b9
aman1357 <amancorp04@gmail.com>
2025-10-04 19:07:52 +0530

    Added Setting icon

--- src/components/navbar/NavbarButtons/Settings.js
+++ src/components/navbar/NavbarButtons/Settings.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { CiSettings } from "react-icons/ci";
+import SettingsModal from "../../modals/SettingsModal";
 
 const Settings = () => {
+    const [openSettings, setOpenSettings]  = useState(false)
+
+    const toggleSettings = () => {
+        setOpenSettings((prevState) => !prevState)
+    }
     return (
-        <div className="icons">
+        <div className="icons" onClick={toggleSettings}>
            <CiSettings />
+            {openSettings && <SettingsModal close={setOpenSettings}/>}
         </div>
 
     )
