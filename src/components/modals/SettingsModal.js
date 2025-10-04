4cf3d7c8a27ca0dbaa3b3d9076c03b6fa8d14a15
aman1357 <amancorp04@gmail.com>
2025-10-04 19:08:59 +0530

    created Modal

--- src/components/modals/SettingsModal.js
+++ src/components/modals/SettingsModal.js
@@ -1,9 +1,22 @@
+import Stats from "../navbar/NavbarButtons/Stats";
 
-const SettingsModal = () => {
+const SettingsModal = ({close}) => {
     return (
-        <div>
-             
-        </div>
-
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <div className="overlay">
+              <div className="stats">
+                <button className="closebtn" onClick={()=>{close(false)}}>X</button>
+                 <h1>Settings</h1>
+                  <p>Profile </p>
+     
+                <div>
+                  <p>Stats</p>
+                  <Stats/>
+                </div>
+              </div>
+            </div>
+          </div>
     )
 }
+
+export default SettingsModal;
