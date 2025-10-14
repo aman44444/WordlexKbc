c221e5bf91b5087c0be2f743b2c52d8acaf64166
aman1357 <amancorp04@gmail.com>
2025-10-14 14:07:41 +0530

    Added style to SignUp

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import "./SignUp.css";
 
 const Signup = () => {
+  const emailRef =  useRef()
+  const passwordRef =  useRef()
+  const passwordConfirmRef =  useRef()
   return (
-    <div>
+    <div className='signup-container'>
         <h2>Sign Up</h2>
-        <form>
+        <form >
             <label>Email</label>
-            <input type='email' placeholder='wordlexkbc@gmail.com' required/>
+            <input type='email' placeholder='wordlexkbc@gmail.com' required ref={emailRef}/>
             <label>Password</label>
-            <input type='password' required/>
+            <input type='password' required ref={passwordRef}/>
             <label>Password Confirmation</label>
-            <input type='password' required/>
+            <input type='password' required ref={passwordConfirmRef}/>
             <button type="submit">Sign Up</button>
         </form>
         <div>
