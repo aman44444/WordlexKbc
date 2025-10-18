b85b41f4f66c49e3560adcbbaeffe68c9f10a684
aman1357 <amancorp04@gmail.com>
2025-10-18 16:19:46 +0530

    recreated  Settings files

--- src/components/Settings/SettingsModal.js
+++ src/components/Settings/SettingsModal.js
@@ -1,20 +1,16 @@
+import ModalWrapper from "../modals/ModalWrapper";
 import Stats from "../navbar/NavbarButtons/Stats";
-import "../Settings/Settings.css"
+
 
 const SettingsModal = ({close}) => {
     return (
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <div className="overlay">
-              <div className="stats">
-                <button className="closebtn" onClick={()=>{close(false)}}>X</button>
-                 <h1>Settings</h1>
+      <ModalWrapper close={close}>
+                <h1>Settings</h1>
                 <div className="Stats-div">
                   <p>Statistics</p>
                   <Stats/>
                 </div>
-              </div>
-            </div>
-          </div>
+      </ModalWrapper>
     )
 }
 
