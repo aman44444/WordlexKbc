784e82d42b5f5c874356b0ecf25f4fa530a16003
aman1357 <amancorp04@gmail.com>
2025-12-12 17:04:26 +0530

    refactor(lifelinesbuttons): add state

--- src/components/lifelines/lifelineButtons/Flip.js
+++ src/components/lifelines/lifelineButtons/Flip.js
@@ -1,44 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../../../App";
 import { generateWordSet } from "../../../utils/Words";
+import { useLifeline } from "./useLifeLineState";
 
 export default function Flip() {
-  const { setCorrectWord, setWordSet, board, setCurrAttempt,gameOver } =
+  const { setCorrectWord, setWordSet, board, setCurrAttempt,gameOver, setBoard } =
     useContext(AppContext);
 
-  const [bDisabled, setBDisabled] = useState(() => {
-    const storedValue = localStorage.getItem("ButtonDisabled");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
+  const {disabled, disable} = useLifeline('flip')
 
-  useEffect(() => {
-    localStorage.setItem("ButtonDisabled", JSON.stringify(bDisabled));
-  }, [bDisabled]);
-
-  const handleClick = (event) => {
-    event.stopPropagation();
-     if (gameOver.gameOver) return; 
-    const newBoard = [...board];
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 6; j++) {
-        newBoard[j][i] = "";
-      }
-    }
+  const handleClick = async () => {
+   if(disabled || gameOver.gameOver) return;
 
     setCurrAttempt({ attempt: 0, letter: 0 });
 
-    generateWordSet().then((words) => {
-      setWordSet(words.wordSet);
-      setCorrectWord(words.todaysWord);
-    });
-    setBDisabled(true);
-    event.currentTarget.id = "dis";
-    console.log("click");
+    const emptyBoard = board.map(row => row.map(() => ""));
+    setBoard(emptyBoard);
+
+    const words = await generateWordSet();
+    setWordSet(words.wordSet)
+    setCorrectWord(words.todaysWord)
+
+    disable();
+    
   };
 
   return (
-    <div className="lifeline" id="" onClick={handleClick} disabled={bDisabled}>
+    <button className="lifeline" onClick={handleClick}  type='button' disabled={disabled}>
       Flip
-    </div>
+    </button>
   );
 }
