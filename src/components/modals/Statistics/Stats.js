db0245d4823c6e7c617cf7ed47ba34bc3cd34968
aman1357 <amancorp04@gmail.com>
2026-02-10 23:43:46 +0530

    refactor: improve folder structure

---
+++ src/components/modals/Statistics/Stats.js
@@ -0,0 +1,25 @@
+import {  useState } from "react";
+import { IoIosStats } from "react-icons/io";
+import Statistics from "./Statistics";
+import { useAuth } from "../../../Context/AuthContext";
+import AuthGate from "../../auth/AuthGate";
+
+const Stats = () => {
+  const [openStats, setOpenStats] = useState(false);
+  const [hover , setHover] = useState(false)
+  const {currentUser} = useAuth()
+
+  return (
+    <div className="icons" onClick={() => setOpenStats(true)}>
+      <IoIosStats 
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        style={{color: hover ? "#528d4e": ""}}
+      />
+      {openStats && (currentUser ?  <Statistics close={setOpenStats}/> : <AuthGate onAuthSuccess={()=> setOpenStats(false)}/>)
+      }
+    </div>
+  );
+};
+
+export default Stats;
